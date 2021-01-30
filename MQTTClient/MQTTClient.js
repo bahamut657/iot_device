0995979af59af52abea21aa691033dcf436acd26
MaDPaPo <jacopo.vichi@gmail.com>
2021-01-30 02:12:11 +0100

    MQTT Connection management

--- MQTTClient/MQTTClient.js
+++ MQTTClient/MQTTClient.js
@@ -1,82 +1,131 @@
-const mqtt = require('mqtt');
-class MQTTClient {
-	constructor(props){
-		this.config = {
-			host:null,
-			port:null,
-			username:null,
-			password:null
-		}
-		this.state={
-			connecting:false,
-			connected:false,
-			subscribedTopicList:[],
-			queues:{
-				onConnect:[]
-			}
-		}
-		Object.keys(props).forEach(key=>this.config[key]=props[key]);
-		this.startup()
-	}
-	set_connected(value){
-		if (this.state.connected !== value){
-			this.state.connected = value;
-			if ( this.state.connected)
-				this.set_connecting(false)
-		}
-	}
-	set_connecting(value){
-		if (this.state.connecting !== value)
-			this.state.connecting = value;
-	}
-	startup() {
-		const {host,port,username,password}=this.config;
-		this.state.client = mqtt.connect({host,port},{username,password});
-		this.sta
-	}
-	check_connection(){
-		return new Promise((resolve,reject)=>this.state.connected ? resolve() : this.wait_connection({resolve,reject}))
-	}
-	wait_connection({resolve,reject}){
-		if (this.state.connecting){
-			this.state.queues.onConnect.push({resolve,reject})
-		else
-			this.connect()
-	}
-	connect(){
-		const {host,port,username,password}=this.config;
-		this.set_connecting(true)
-		this.state.client = mqtt.connect({host,port},{username,password});
-		this.set_timeout("connect");
-		this.state.client.on("connect",(err)=>{
-			if (err){
-			}else{
-			}
-		})
-	}
-	publish({topic,message="",qos=0}){
-		return new Promise((resolve,reject)=>
-			this.check_connection().then(()=>{
-				this.state.client.publish(topic,message,{qos},err=>err?reject(err) : resolve());
-			})
-		)
-	}
+const mqtt = require("mqtt");
 
+const CONNECTION_TIMEOUT = 5000;
+const IDLE_TIMEOUT = 10000;
+
+class MQTTClient {
+  constructor(props) {
+    this.config = {
+      host: null,
+      port: null,
+      username: null,
+      password: null,
+      timeout: {
+        connect: CONNECTION_TIMEOUT,
+        idle: IDLE_TIMEOUT,
+      },
+      ...props,
+    };
+    this.state = {
+      connecting: false,
+      connected: false,
+      subscribedTopicList: [],
+      queues: {
+        onConnect: [],
+      },
+      timeout: {
+        connect: null,
+        idle: null,
+      },
+    };
+
+    this.startup();
+  }
+  set_connected(value) {
+    if (this.state.connected !== value) {
+      this.state.connected = value;
+      if (this.state.connected) this.set_connecting(false);
+    }
+  }
+  set_connecting(value) {
+    if (this.state.connecting !== value) this.state.connecting = value;
+  }
+  clear_timeout({ type }) {
+    clearTimeout(this.state.timeout[type]);
+  }
+  set_timeout({ type, cb }) {
+    this.clear_timeout({ type });
+    this.state.timeout[type] = setTimeout(() => {
+      cb();
+    }, this.config.timeout[type]);
+  }
+  startup() {
+    const { host, port, username, password } = this.config;
+    this.state.client = mqtt.connect({ host, port }, { username, password });
+    this.sta;
+  }
+  check_connection() {
+    return new Promise((resolve, reject) =>
+      this.state.connected
+        ? resolve()
+        : this.wait_connection({ resolve, reject })
+    );
+  }
+  wait_connection({ resolve, reject }) {
+    this.state.queues.onConnect.push({ resolve, reject });
+    if (!this.state.connecting) this.connect();
+  }
+  connect() {
+    const { host, port, username, password } = this.config;
+    this.set_connecting(true);
+    this.state.client = mqtt.connect({ host, port }, { username, password });
+    this.set_timeout({
+      type: "connect",
+      cb: () => {
+        this.set_connecting(false);
+        this.state.queues.onConnect.forEach((promise) =>
+          promise.reject(
+            new Error(
+              `Timeout connecting MQTT to ${this.config.host}:${this.config.port}`
+            )
+          )
+        );
+        this.state.queues.onConnect = [];
+      },
+    });
+    this.state.client.on("connect", (err) => {
+      this.clear_timeout({
+        type: "connect",
+      });
+      if (!err) this.set_connected(true);
+      else this.set_connecting(false);
+      this.state.queues.onConnect.forEach((promise) =>
+        err ? promise.reject(err) : promise.resolve()
+      );
+      this.state.queues.onConnect = [];
+    });
+    this.state.client.on("close", () => this.set_connected(false));
+    this.state.client.on("message", (topic, message) =>
+      this.recv({ topic, message })
+    );
+  }
+
+  publish({ topic, message = "", qos = 0 }) {
+    return new Promise((resolve, reject) =>
+      this.check_connection().then(() => {
+        this.state.client.publish(topic, message, { qos }, (err) =>
+          err ? reject(err) : resolve()
+        );
+      })
+    );
+  }
+
+  recv({ topic, message }) {}
 }
 module.exports = { MQTTClient };
 
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var client = mqtt.connect("mqtt://test.mosquitto.org");
 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
+client.on("connect", function () {
+  client.subscribe("presence", function (err) {
     if (!err) {
-      client.publish('presence', 'Hello mqtt')
+      client.publish("presence", "Hello mqtt");
     }
-  })
-})
+  });
+});
 
-client.on('message', function (topic, message) {
+client.on("message", function (topic, message) {
   // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+  console.log(message.toString());
+  client.end();
+});
--- MQTTClient/index.js
+++ MQTTClient/index.js
@@ -1,4 +1,2 @@
-const MQTTClient = require("./MQTTClient");
-const service = MQTTClient(JSON.parse(process.env.service_config))
-
-
+const { MQTTClient } = require("./MQTTClient");
+const service = MQTTClient(JSON.parse(process.env.service_config));
